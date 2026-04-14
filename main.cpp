b6b3aa6386181c4846d4c7a3edadaf646f311bb5
Karim Doss <karimdoss@aucegypt.edu>
2026-04-14 14:35:14 +0200

    BONUS

--- main.cpp
+++ main.cpp
@@ -2,38 +2,31 @@
 #include <map>
 #include <string>
 #include <stdexcept>
-
+#include "bcrypt/BCrypt.hpp"
 #include "utils.hpp"
-
 int main() {
     std::map<std::string, std::string> users;
     int choice = -1;
     bool logged_in = false;
-
     while (choice != 0) {
         std::cout << "1. Sign up\n";
         std::cout << "2. Login\n";
-
         if (logged_in) {
             std::cout << "3. Logout\n";
         }
-
         std::cout << "0. Exit\n";
         std::cout << "Enter choice: ";
         std::cin >> choice;
-
         if (choice == 1 && !logged_in) {
             std::string email;
             std::string password;
-
             std::cout << "Enter email: ";
             std::cin >> email;
-
             try {
                 check_email(email);
                 std::cout << "Enter password: ";
                 std::cin >> password;
-                users[email] = password;
+                users[email] = BCrypt::generateHash(password);
                 std::cout << "Sign up successful\n";
             } catch (std::invalid_argument& e) {
                 std::cout << e.what() << "\n";
@@ -41,13 +34,13 @@ int main() {
         } else if (choice == 2 && !logged_in) {
             std::string email;
             std::string password;
-
             std::cout << "Enter email: ";
             std::cin >> email;
             std::cout << "Enter password: ";
             std::cin >> password;
 
-            if (users.find(email) != users.end() && users[email] == password) {
+            if (users.find(email) != users.end() &&
+                BCrypt::validatePassword(password, users[email])) {
                 logged_in = true;
                 std::cout << "Login successful\n";
             } else {
@@ -59,9 +52,7 @@ int main() {
         } else if (choice != 0) {
             std::cout << "Invalid choice\n";
         }
-
         std::cout << "\n";
     }
-
-    return 0;
+   return 0;
 }
--- utils.cpp
+++ utils.cpp
@@ -3,9 +3,7 @@
 #include <stdexcept>
 
 void check_email(const std::string& email) {
-    std::regex pattern(".+@.+\\..+");
-
-    if (!std::regex_match(email, pattern)) {
-        throw std::invalid_argument("Invalid email format");
-    }
-}
+ std::regex pattern(".+@.+\\..+");
+ if (!std::regex_match(email, pattern)) {
+ throw std::invalid_argument("Invalid email format");
+ }}
